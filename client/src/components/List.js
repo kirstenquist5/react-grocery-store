0bc68fffcbfd5d468711de89cf154a92c570af22
Kirsten Stenquist <kirstenquist5@gmail.com>
2018-12-27 22:01:04 -0700

    how do I add items

--- client/src/components/List.js
+++ client/src/components/List.js
@@ -1,53 +1,67 @@
-import React from 'react'
-import axios from 'axios'
+import React from 'react';
+import axios from 'axios';
 import Form from './Form';
 
 class List extends React.Component {
-  state = { list: {}, edit: false }
+  state = { list:{}, items: [{}] }
 
   componentDidMount() {
     axios.get(`/api/lists/${this.props.match.params.id}`)
       .then(res => {
-        this.setState({ list: res.data })
+        this.setState({ list:res.data })
+        console.log(res.data);
       })
+
+    axios.get(`/api/lists/${this.props.match.params.id}/items`)
+    .then(res => {
+      this.setState({ items:res.data })
+      console.log(res.data);
+    })
   }
 
-  toggleEdit = () => {
-    this.setState(state => {
-      return { edit: !this.state.edit }
-    })
+  submit = (item) => {
+    
+    axios.post('/api/items', { item })
+      .then(res => {
+        this.setState({ list: [res.data, ...this.state.lists]})
+      })
+    console.log(item);
   }
 
   showList = () => {
-    const { list: { name } } = this.state
+    // // const { list: { name } } = this.state;
+    // const { items: { itemName } } = this.state;
     return (
       <div>
-        <h1>{name}</h1>
+        <h1>{this.state.list.name}</h1>
+        <br />
+        {/* <h4>{this.state.items[0].name}</h4> */}
+        {this.state.items.map(i => {
+          return (
+          <ul key={i.id}>
+            <li>
+              <h4>
+                {i.name}
+              </h4>
+            </li>
+          </ul>
+          )
+        })}
+        <h3>Add new item</h3>
+        <Form submit={this.submit}/>
       </div>
     )
   }
 
-  edit = () => {
-    return <Form {...this.state.list} submit={this.submit} />
-  }
-
-  submit = (list) => {
-    axios.put(`/api/lists/${this.props.match.params.id}`, { list })
-    .then(res => {
-      this.setState({ list: res.data, edit: false })
-    })
-  }
+  // this.state.lists.map(p => {
 
   render() {
-    const { edit } = this.state
     return (
       <div>
-        {edit ? this.edit() : this.showList()}
-        <button onClick={this.toggleEdit}>{ edit ? 'Cancel' : 'Edit' }</button>
+        {this.showList()}
       </div>
     )
   }
-
 }
 
-export default List
+export default List;
